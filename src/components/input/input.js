37038447f550f4d9e2a804ee65a9a89c967f4425
System Administrator <root@Deepaks-MacBook-Pro.local>
2020-05-01 17:20:27 +0530

    contact us from validation

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -4,11 +4,11 @@ import styled from 'styled-components'
 import { colors } from 'config'
 
 const Input = (props) => {
-  const { label } = props
+  const { label, type } = props
   return (
     <Fragment>
       {label ? <Label>{label}</Label> : null}
-      <InputTag />
+      {(type === 'input') ? <InputTag /> : <Textarea />}
     </Fragment>
   )
 }
@@ -18,32 +18,43 @@ export default Input
 const { blue } = colors
 
 export const InputTag = styled.input`
-  width: 100%;
-  height: 55px;
-  padding: 20px;
-  border-radius: 29px;
-  outline: none;
-  border: 1px solid #e4e4e4;
-  margin-bottom: 47px;
+    width: 100%;
+    height: 55px;
+    padding: 20px;
+    border-radius: 29px;
+    outline: none;
+    border: 1px solid #e4e4e4;
+    margin-bottom: 47px;
+`
+
+export const Textarea = styled.input`
+    width: 100%;
+    padding: 20px;
+    resize: none;
+    height: 155px;
+    border-radius: 29px;
+    outline: none;
+    border: 1px solid #e4e4e4;
+    margin-bottom: 47px;
 `
 
 export const Label = styled.label`
-  text-align: left;
-  font-size: 18px;
-  font-weight: bold;
-  font-stretch: normal;
-  font-style: normal;
-  line-height: normal;
-  letter-spacing: -0.43px;
-  width: 100%;
-  color: ${blue};
-  margin-bottom: 17px;
+    text-align: left;
+    font-size: 18px;
+    font-weight: bold;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: -0.43px;
+    width: 100%;
+    color: ${blue};
+    margin-bottom: 17px;
 `
 
 export const H1 = styled.h1`
-  color: ${blue};
-  font-size: 34px;
-  font-family: Arial;
-  font-weight: bold;
-  margin: 10px 0px;
+    color: ${blue};
+    font-size: 34px;
+    font-family: Arial;
+    font-weight: bold;
+    margin: 10px 0px;
 `
--- src/screens/ContactUs.js
+++ src/screens/ContactUs.js
@@ -1,19 +1,25 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import PageTitle from 'components/PageTitle'
 import Input from 'components/input/input'
-import Textarea from 'components/input/textarea'
 import styled from 'styled-components'
 import { Button } from 'react-bootstrap'
 
 const ContactUs = () => {
+  const [isSubmit, setSubmitOn] = useState(false);
+  const [name, setname] = useState('');
+  const [email, setemail] = useState('');
+  const [message, setmessage] = useState('');
+  const setValue = (event) => {
+    console.log(event);
+  }
   return (
     <Fragment>
       <PageTitle title="Get in touch" />
       <ContactForm>
-        <Input label="Name" />
-        <Input label="Email" />
-        <Textarea label="Message" />
-        <Button>Send</Button>
+        <Input type="input" value={name} onkeyup={(event) => setValue(event)} label="Name" />
+        <Input type="input" value={email} onkeyup={(event) => setValue(event)} label="Email" />
+        <Input type="textarea" value={message} onkeyup={(event) => setValue(event)} label="Message" />
+        <Button disabled={!isSubmit}>Send</Button>
       </ContactForm>
     </Fragment>
   )
@@ -22,13 +28,13 @@ const ContactUs = () => {
 export const ContactForm = styled.div`
   width: 40%;
   text-align: center;
-  button {
+  button, butoon:disabled {
     width: 128px;
     line-height: initial;
     height: 30px;
     border-radius: 15px;
     border: none;
-    background-color: #00a778;
+    background-color: #00a778 !important;
   }
 `
 
